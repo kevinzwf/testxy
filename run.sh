a14d712b4bf7df48c04277207284015e11f8a8bc
kevin <kevin.zwf@gmail.com>
2017-06-08 17:01:30 +0800

    run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-/usr/sbin/httpd -DFOREGROUND &
-
 /etc/init.d/mysql start
 
 /usr/bin/mysqladmin -uroot password 'test@1234'
 
+mysql -uroot -p'test@1234' < /tmp/mysql.fine.sql
+
+/usr/sbin/httpd -DFOREGROUND &
