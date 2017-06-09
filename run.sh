95f4d2772547ec08aeb0b7365062edf57c37be1f
kevin <kevin.zwf@gmail.com>
2017-06-09 13:50:58 +0800

    Create run.sh

--- run.sh
+++ run.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-tar -xzf /finecms.tgz -C /var/www/html/ && chown -R apache. /var/www/html/
 /etc/init.d/mysql start
 
 /usr/bin/mysqladmin -uroot password 'test@1234'
 
-mysql -uroot -p'test@1234' < /tmp/mysql.fine.sql
+mysql -uroot -p'test@1234' < /mysql.fine.sql
 
-/usr/sbin/httpd -DFOREGROUND &
+/usr/sbin/httpd -DFOREGROUND
